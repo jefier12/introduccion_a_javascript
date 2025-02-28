8716e995751fd8c0a0b4db152ea6636bdc9b10ed
jefier12 <jefierbriceno12@gmail.com>
2025-02-28 10:01:11 -0500

    inicio del taller

--- ejemplos.js
+++
@@ -1,7 +0,0 @@
-// o Que son los temporizadores setimeout y setinterval (de un ejemplo)
-// o Que es la asincronía y el evento loop (de un ejemplo)
-// o Que son los callbacks (de un ejemplo)
-// o Que son las promesas (de un ejemplo)
-// o Que son las funciones asíncronas (async / await) (de un ejemplo)
-
-
